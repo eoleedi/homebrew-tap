122aa7fb29a8ea9e75e327765407ef793149216e
蔡鳳駿 <eoleedimin@gmail.com>
2025-03-15 20:16:54 +0800

    add WiFiLocControl formula

---
+++ Formula/wifi-loc-control.rb
@@ -0,0 +1,32 @@
+class WifiLocControl < Formula
+  desc "Allows you to change your macOS network location automatically based on the Wi-Fi network name (SSID) you are connected to"
+  homepage "https://github.com/vborodulin/wifi-loc-control"
+  url "https://github.com/vborodulin/wifi-loc-control/archive/refs/tags/1.0.2.tar.gz"
+  sha256 "334c2d63c1c371cebdc20090d242f3d4f8a01ea20bd8d0134e050bf413a12f09"
+  license "MIT"
+  version "1.0.2"
+
+  depends_on :macos
+
+  def install
+    bin.install "wifi-loc-control.sh" => "wifi-loc-control"
+
+    # modify the plist file
+    inreplace "WifiLocControl.plist", "/usr/local/bin/wifi-loc-control.sh", "#{bin}/wifi-loc-control"
+
+    # install the plist file
+    prefix.install "WifiLocControl.plist" => "application.com.wifi-loc-control.plist"
+  end
+
+  service do
+    name macos: "application.com.wifi-loc-control"
+  end
+
+  def caveats
+    <<~EOS
+      To start wifi-loc-control now and restart at login:
+        brew services start wifi-loc-control
+    EOS
+  end
+
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# homebrew-tap
+
+This is a Homebrew tap for interetsing tools that are not in the main Homebrew repository.
+## Usage
+1. Install [WiFiLocControl](https://github.com/vborodulin/wifi-loc-control)
+    ```bash
+    brew install eoleedi/tap/wifi-loc-control
+    ```
