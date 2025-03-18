0b9337ac418a1cb169ba06502b807622078534d9
蔡鳳駿 <eoleedimin@gmail.com>
2025-03-18 21:57:07 +0800

    fix: add test & improv styling

--- Formula/wifi-loc-control.rb
+++ Formula/wifi-loc-control.rb
@@ -1,5 +1,5 @@
 class WifiLocControl < Formula
-  desc "Allows you to change your macOS network location automatically based on the Wi-Fi network name (SSID) you are connected to"
+  desc "Change macOS network location based on the Wi-Fi network name (SSID)"
   homepage "https://github.com/vborodulin/wifi-loc-control"
   url "https://github.com/vborodulin/wifi-loc-control/archive/refs/tags/1.0.2.tar.gz"
   sha256 "334c2d63c1c371cebdc20090d242f3d4f8a01ea20bd8d0134e050bf413a12f09"
@@ -11,7 +11,7 @@ class WifiLocControl < Formula
     bin.install "wifi-loc-control.sh" => "wifi-loc-control"
 
     # Modify the Program Path to brew installed one
-    inreplace "WifiLocControl.plist", "/usr/local/bin/wifi-loc-control.sh", "#{bin}/wifi-loc-control"
+    inreplace "WifiLocControl.plist", "/usr/local/bin/wifi-loc-control.sh", bin/"wifi-loc-control"
 
     # Install the plist file
     prefix.install "WifiLocControl.plist" => "application.com.wifi-loc-control.plist"
@@ -28,4 +28,8 @@ class WifiLocControl < Formula
     EOS
   end
 
+  test do
+    assert_predicate bin/"wifi-loc-control", :executable?
+    assert_path_exists prefix/"application.com.wifi-loc-control.plist"
+  end
 end
