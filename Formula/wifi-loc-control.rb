b90678f9b8d3d708c1eb11a43264a7d5e4a64fb4
蔡鳳駿 <eoleedimin@gmail.com>
2025-08-27 23:27:04 +0200

    wifi-loc-control 1.0.4

--- Formula/wifi-loc-control.rb
+++ Formula/wifi-loc-control.rb
@@ -1,8 +1,8 @@
 class WifiLocControl < Formula
   desc "Change macOS network location based on the Wi-Fi network name (SSID)"
   homepage "https://github.com/vborodulin/wifi-loc-control"
-  url "https://github.com/vborodulin/wifi-loc-control/archive/refs/tags/1.0.3.tar.gz"
-  sha256 "7e0669fe8d6121ce8733dce0a8516a79974eaad9ce4399775cef40b3c20218fa"
+  url "https://github.com/vborodulin/wifi-loc-control/archive/refs/tags/1.0.4.tar.gz"
+  sha256 "fc5f468a605d56bd132b55106a594e6452af9394cd97d88d89e4318ef57c27b3"
   license "MIT"
 
   depends_on :macos
